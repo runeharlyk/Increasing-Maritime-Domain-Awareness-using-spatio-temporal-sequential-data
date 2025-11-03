b313032435248db6e89a7537f524f7399479ebac
Rune Harlyk <rune@harlyk.dk>
2025-11-03 13:49:19 +0100

    Formats and adds spell check dict

--- ais_to_parquet.py
+++ ais_to_parquet.py
@@ -20,31 +20,48 @@ def fn(file_path, out_path):
     # Remove errors
     bbox = [60, 0, 50, 20]
     north, west, south, east = bbox
-    df = df[(df["Latitude"] <= north) & (df["Latitude"] >= south) & (df["Longitude"] >= west) & (
-            df["Longitude"] <= east)]
+    df = df[
+        (df["Latitude"] <= north)
+        & (df["Latitude"] >= south)
+        & (df["Longitude"] >= west)
+        & (df["Longitude"] <= east)
+    ]
 
-    df = df[df["Type of mobile"].isin(["Class A", "Class B"])].drop(columns=["Type of mobile"])
+    df = df[df["Type of mobile"].isin(["Class A", "Class B"])].drop(
+        columns=["Type of mobile"]
+    )
     df = df[df["MMSI"].str.len() == 9]  # Adhere to MMSI format
     df = df[df["MMSI"].str[:3].astype(int).between(200, 775)]  # Adhere to MID standard
 
     df = df.rename(columns={"# Timestamp": "Timestamp"})
-    df["Timestamp"] = pandas.to_datetime(df["Timestamp"], format="%d/%m/%Y %H:%M:%S", errors="coerce")
+    df["Timestamp"] = pandas.to_datetime(
+        df["Timestamp"], format="%d/%m/%Y %H:%M:%S", errors="coerce"
+    )
 
-    df = df.drop_duplicates(["Timestamp", "MMSI", ], keep="first")
+    df = df.drop_duplicates(
+        [
+            "Timestamp",
+            "MMSI",
+        ],
+        keep="first",
+    )
 
     def track_filter(g):
         len_filt = len(g) > 256  # Min required length of track/segment
         sog_filt = 1 <= g["SOG"].max() <= 50  # Remove stationary tracks/segments
-        time_filt = (g["Timestamp"].max() - g["Timestamp"].min()).total_seconds() >= 60 * 60  # Min required timespan
+        time_filt = (
+            g["Timestamp"].max() - g["Timestamp"].min()
+        ).total_seconds() >= 60 * 60  # Min required timespan
         return len_filt and sog_filt and time_filt
 
     # Track filtering
     df = df.groupby("MMSI").filter(track_filter)
-    df = df.sort_values(['MMSI', 'Timestamp'])
+    df = df.sort_values(["MMSI", "Timestamp"])
 
     # Divide track into segments based on timegap
-    df['Segment'] = df.groupby('MMSI')['Timestamp'].transform(
-        lambda x: (x.diff().dt.total_seconds().fillna(0) >= 15 * 60).cumsum())  # Max allowed timegap
+    df["Segment"] = df.groupby("MMSI")["Timestamp"].transform(
+        lambda x: (x.diff().dt.total_seconds().fillna(0) >= 15 * 60).cumsum()
+    )  # Max allowed timegap
 
     # Segment filtering
     df = df.groupby(["MMSI", "Segment"]).filter(track_filter)
@@ -68,9 +85,10 @@ def fn(file_path, out_path):
     pyarrow.parquet.write_to_dataset(
         table,
         root_path=out_path,
-        partition_cols=["MMSI",  # "Date",
-                        "Segment",  # "Geocell"
-                        ]
+        partition_cols=[
+            "MMSI",  # "Date",
+            "Segment",  # "Geocell"
+        ],
     )
 
 
